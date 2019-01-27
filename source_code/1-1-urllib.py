50bf5589cd7e2531b74b8b71e0a8d87f9314a433
diandianyangyi <2007303280@163.com>
2019-01-27 22:27:45 +0800

    add some explation

--- source_code/1-1-urllib.py
+++ source_code/1-1-urllib.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 html = urlopen("https://morvanzhou.github.io/static/scraping/basic-structure.html").read().decode('utf-8')
 print(html)
 
-
+#引入正则表达式
 import re
 res = re.findall(r"<title>(.+?)</title>", html)
 print("\nPage title is: ", res[0])
--- source_code/2-1-beautifulsoup-basic.py
+++ source_code/2-1-beautifulsoup-basic.py
@@ -1,3 +1,6 @@
+#引入xml 解析器，beautifulSoup is a library for pulling data out of HTML and XML files.
+#It works with your favorite parset to provide idiomatic(惯用) ways of navigating(导航),searching,and
+#modifying the parse tree. 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -9,6 +12,10 @@ print(soup.h1)
 print('\n', soup.p)
 
 all_href = soup.find_all('a')
+
+#机器学习中的语句，一般不这么用
+#语句featList = [example[i] for example in dataSet]作用为： 
+#将dataSet中的数据按行依次放入example中，然后取得example中的example[i]元素，放入列表featList中
 all_href = [l['href'] for l in all_href]
 print('\n', all_href)
 
